17b400671b2c4cded0208b8d5ba7c392e269375a
Aaron Hurley <aaron.hurley@atu.ie>
2024-02-16 16:19:37 +0000

    new front page with parallax, and old index page now shop page

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+/* this calculates and updates the total number of items in the basket*/
+let calculate = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((z) => z.item).reduce((x, y) => x + y, 0);
+}
+
+calculate();
