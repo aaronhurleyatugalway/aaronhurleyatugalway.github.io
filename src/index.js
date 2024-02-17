37b47a42cf92162f36c56c9d4a0ce25e229498b9
Aaron Hurley <aaron.hurley@atu.ie>
2024-02-17 09:54:48 +0000

    add shopping cart to bottom of index page

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ let basket = JSON.parse(localStorage.getItem("data")) || [];
 /* this calculates and updates the total number of items in the basket*/
 let calculate = () => {
     let cartIcon = document.getElementById("cartAmount");
-    cartIcon.innerHTML = basket.map((z) => z.item).reduce((x, y) => x + y, 0);
+    let cartIcon2 = document.getElementById("cartAmount2");
+    let numberOfItems = basket.map((z) => z.item).reduce((x, y) => x + y, 0);
+    cartIcon.innerHTML = numberOfItems;
+    cartIcon2.innerHTML = numberOfItems;
 }
 
 calculate();
-
-let navbar2 = document.getElementById("navbar2");
-
-navbar2.innerHTML = `<h2><a href="index.html"><img src="images/locologo.jpg" width="160px"></h2></a>
-    <a href="shop.html" class="shoplink">SHOP <i class="bi bi-shop"></i></a>
-`
